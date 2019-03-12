b5c4a58d82c866764e54163b8957c29dee5af6d3
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-03-12 13:21:08 -0700

    added in new push method for DLL

--- main.js
+++ main.js
@@ -52,132 +52,132 @@ class Point {
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Single Liked List // BIG O NOTATION O(1) to O(N)
 
-  class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-class SingleLinkedList {
-    constructor() {
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
-    }
-    push(val) {
-        var newNode = new Node(val)
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        }
-        else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++
-        return this;
-    }
-    pop(){
-        if(!this.head) return undefined;
-        var current = this.head;
-        var newTail = current;
-        while(current.next) {
-            newTail = current;
-            current = current.next;
-        }
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        if(this.length === 0) {
-            this.head = null;
-            this.tsil = null;
-        }
-        return current;
-    }
-    shift(){
-        if(!this.head) return undefined;
-        var currentHead = this.head;
-        this.head = currentHead.next;
-        this.length--;
-        if(this.length === o) {
-            this.tail = null;
-        }
-        return currentHead;
-    }
-    unshift(val){
-        var newNode = new Node(val);
-        if(!this.head) {
-            this.head = new Node;
-            this.tail = this.head;
-        } else {
-        newNode.next = this.head;
-        this.head = newNode;
-        }
-        this.length++
-        return this;
-    }
-    get(index) {
-        if(index < 0 || index >= this.length) return null;
-        var count = 0;
-        var current = this.head;
-        while(count !== index) {
-            current = current.next;
-            count++ 
-        }
-        return current;
-    }
-    set(index, val) {
-        var foundNode = this.get(index);
-        if(foundNode){
-            foundNode.val = val;
-            return true;
-        }
-        return false;
-    }
-    insert(index, val) {
-        //!!something <---- equals true(bollean)
-        if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);
-        if(index === 0) return !!this.unshift(val);
-        var newNode = new Node(val);
-        var prev = this.get(index - 1);
-        var temp = prev.next;
-        prev.next = newNode;
-        newNode.next = temp;
-        this.length++
-        return true;
-    }
-    remove(index) {
-        if(index < 0 || index >= this.length) return undefined;
-        if(index === 0) return this.shift();
-        if(index === this.length - 1) return this.pop();
-        var prevNode = this.get(index - 1);
-        var removed = prevNode.next;
-        prevNode.next = removed.next;
-        this.length--;
-        return removed;
-    }
-    reverse() {
-        var node = this.head;
-        this.head = this.tail;
-        this.tail = node;
-        var prev = null;
-        var next;
-        for(var i = 0; i < this.length; i++) {
-            next = node.next;
-            node.next = prev;
-            prev = node;
-            node = next;
-        }
-        return this;
-    }
- }
+//   class Node {
+//     constructor(val) {
+//         this.val = val;
+//         this.next = null;
+//     }
+// }
+// class SingleLinkedList {
+//     constructor() {
+//         this.length = 0;
+//         this.head = null;
+//         this.tail = null;
+//     }
+//     push(val) {
+//         var newNode = new Node(val)
+//         if(!this.head) {
+//             this.head = newNode;
+//             this.tail = this.head;
+//         }
+//         else {
+//             this.tail.next = newNode;
+//             this.tail = newNode;
+//         }
+//         this.length++
+//         return this;
+//     }
+//     pop(){
+//         if(!this.head) return undefined;
+//         var current = this.head;
+//         var newTail = current;
+//         while(current.next) {
+//             newTail = current;
+//             current = current.next;
+//         }
+//         this.tail = newTail;
+//         this.tail.next = null;
+//         this.length--;
+//         if(this.length === 0) {
+//             this.head = null;
+//             this.tsil = null;
+//         }
+//         return current;
+//     }
+//     shift(){
+//         if(!this.head) return undefined;
+//         var currentHead = this.head;
+//         this.head = currentHead.next;
+//         this.length--;
+//         if(this.length === o) {
+//             this.tail = null;
+//         }
+//         return currentHead;
+//     }
+//     unshift(val){
+//         var newNode = new Node(val);
+//         if(!this.head) {
+//             this.head = new Node;
+//             this.tail = this.head;
+//         } else {
+//         newNode.next = this.head;
+//         this.head = newNode;
+//         }
+//         this.length++
+//         return this;
+//     }
+//     get(index) {
+//         if(index < 0 || index >= this.length) return null;
+//         var count = 0;
+//         var current = this.head;
+//         while(count !== index) {
+//             current = current.next;
+//             count++ 
+//         }
+//         return current;
+//     }
+//     set(index, val) {
+//         var foundNode = this.get(index);
+//         if(foundNode){
+//             foundNode.val = val;
+//             return true;
+//         }
+//         return false;
+//     }
+//     insert(index, val) {
+//         //!!something <---- equals true(bollean)
+//         if(index < 0 || index > this.length) return false;
+//         if(index === this.length) return !!this.push(val);
+//         if(index === 0) return !!this.unshift(val);
+//         var newNode = new Node(val);
+//         var prev = this.get(index - 1);
+//         var temp = prev.next;
+//         prev.next = newNode;
+//         newNode.next = temp;
+//         this.length++
+//         return true;
+//     }
+//     remove(index) {
+//         if(index < 0 || index >= this.length) return undefined;
+//         if(index === 0) return this.shift();
+//         if(index === this.length - 1) return this.pop();
+//         var prevNode = this.get(index - 1);
+//         var removed = prevNode.next;
+//         prevNode.next = removed.next;
+//         this.length--;
+//         return removed;
+//     }
+//     reverse() {
+//         var node = this.head;
+//         this.head = this.tail;
+//         this.tail = node;
+//         var prev = null;
+//         var next;
+//         for(var i = 0; i < this.length; i++) {
+//             next = node.next;
+//             node.next = prev;
+//             prev = node;
+//             node = next;
+//         }
+//         return this;
+//     }
+//  }
 
-var list = new SingleLinkedList()
-list.push('Zero')
-list.push('One')
-list.push('Two')
-list.push('Three')
+// var list = new SingleLinkedList()
+// list.push('Zero')
+// list.push('One')
+// list.push('Two')
+// list.push('Three')
 
 // var first = new Node("First")
 // first.next = new Node("Second")
@@ -200,8 +200,22 @@ class DoubleLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        var newNode = new Node(val);    
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++
+        return this;
+    }
 }
 
+var list = new DoubleLinkedList()
 
 
 
