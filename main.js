b9752b1f37d54babc2e7f8c154ccb4d0f758e028
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-03-12 13:45:00 -0700

    added in shift method

--- main.js
+++ main.js
@@ -227,11 +227,26 @@ class DoubleLinkedList {
         this.length--;
         return poppedNode;
     }
+    shift() {
+        if(this.length === 0) return undefined;
+        var oldHead = this.head;
+        if(this.length === 1) {
+            this.head === null;
+            this.tail === null;
+        } else {
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
 }
 
 var list = new DoubleLinkedList()
 list.push(1)
 list.push(2)
+list.push(3)
 
 
 
