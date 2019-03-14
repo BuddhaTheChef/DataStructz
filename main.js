b4ea6eff97e7c62b318af18fd0f8377e28be0d5b
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-03-13 18:18:11 -0700

    added in set for DLL

--- main.js
+++ main.js
@@ -275,6 +275,14 @@ class DoubleLinkedList {
       }
       return current;
     }
+    set(index, val) {
+        var foundNode = this.get(index);
+        if(foundNode !== null) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
 
 var list = new DoubleLinkedList()
