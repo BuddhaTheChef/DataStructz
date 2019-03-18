8153e245e486dfc5592bfd5bc9cf9269e40584d3
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-03-18 15:11:04 -0700

    added in insert method for BST

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-class Student {
-    constructor(firstName, lastName, year){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.grade = year;
-        this.tardies = 0;
-        this.scores = [];
-    }
-    fullName(){
-        return `Your full name is ${this.firstName} ${this.lastName}`;
-    }
-    markLate(){
-        this.tardies += 1;
-        if(this.tardies >= 3) {
-            return "YOU ARE EXPELLED!!!!"
-        }
-        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
-    }
-    addScore(score){
-        this.scores.push(score);
-        return this.scores
-    }
-    calculateAverage(){
-        let sum = this.scores.reduce(function(a,b){return a+b;})
-        return sum/this.scores.length;
-    }  
-}
+// class Student {
+//     constructor(firstName, lastName, year){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.grade = year;
+//         this.tardies = 0;
+//         this.scores = [];
+//     }
+//     fullName(){
+//         return `Your full name is ${this.firstName} ${this.lastName}`;
+//     }
+//     markLate(){
+//         this.tardies += 1;
+//         if(this.tardies >= 3) {
+//             return "YOU ARE EXPELLED!!!!"
+//         }
+//         return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+//     }
+//     addScore(score){
+//         this.scores.push(score);
+//         return this.scores
+//     }
+//     calculateAverage(){
+//         let sum = this.scores.reduce(function(a,b){return a+b;})
+//         return sum/this.scores.length;
+//     }  
+// }
 
-let firstStudent = new Student("Colt", "Steele",1);
-let secondStudent = new Student("Blue", "Steele",2);
-/////////////////////////////////////////////////////////////////
+// let firstStudent = new Student("Colt", "Steele",1);
+// let secondStudent = new Student("Blue", "Steele",2);
+// /////////////////////////////////////////////////////////////////
 
-class Point {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-    }
+// class Point {
+//     constructor(x, y) {
+//       this.x = x;
+//       this.y = y;
+//     }
   
-    static distance(a, b) {
-      const dx = a.x - b.x;
-      const dy = a.y - b.y;
+//     static distance(a, b) {
+//       const dx = a.x - b.x;
+//       const dy = a.y - b.y;
   
-      return Math.hypot(dx, dy);
-    }
-  }
+//       return Math.hypot(dx, dy);
+//     }
+//   }
   
-  const p1 = new Point(5, 5);
-  const p2 = new Point(10, 10);
+//   const p1 = new Point(5, 5);
+//   const p2 = new Point(10, 10);
   
-  console.log(Point.distance(p1, p2)); // 7.0710678118654755
+//   console.log(Point.distance(p1, p2)); // 7.0710678118654755
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Single Liked List // BIG O NOTATION O(1) to O(N)
@@ -473,13 +473,44 @@ class BST {
     constructor() {
         this.root = null;
     }
+    insert(value) {
+        var newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while(true) {
+                if(value === current.value) return undefined;
+                if(value < current.value) {
+                    if(current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                }
+                else if(value > current.value) {
+                    if(current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
 
-var tree = new BST();
-tree.root = new Node(9);
-tree.root.left = new Node(2);
-tree.root.right = new Node(12);
-tree.root.left.right = new Node(3);
+
+
+// Manual Insert 
+ var tree = new BST();
+ tree.root = new Node(9);
+ tree.root.left = new Node(2);
+ tree.root.right = new Node(12);
+ tree.root.left.right = new Node(3);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
