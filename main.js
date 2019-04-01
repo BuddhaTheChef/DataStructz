be353c04c625d965099aeac2b790fe6deecff1f5
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-04-01 15:49:57 -0700

    implemented itterative solution for graph

--- main.js
+++ main.js
@@ -1005,6 +1005,27 @@ class Graph{
     })(start)
     return result;
   }
+  depthFirstItteritave(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while(stack.length) {
+      console.log(stack)
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if(!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor)
+        }
+      })
+    }
+    return result;
+  }
 }
 
 
