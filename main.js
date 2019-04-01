0be919420cb561995bd51b42d8801317653b7955
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-04-01 15:35:48 -0700

    added in recursive function for graph

--- main.js
+++ main.js
@@ -988,17 +988,54 @@ class Graph{
     }
     delete this.adjacencyList[vertex]
   }
+  depthFirstRecursive(start){
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if(!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor=> {
+        if(!visited[neighbor]){
+          return dfs(neighbor)
+        }
+      })
+    })(start)
+    return result;
+  }
 }
 
 
 var g = new Graph();
-g.addVertex('Tokyo')
-g.addVertex('Seoul')
-g.addVertex('America')
-g.addVertex('Hawaii')
-g.addEdge('Tokyo', 'Hawaii')
-g.addEdge('Tokyo', 'Seoul')
-g.addEdge('Hawaii', 'America')
-g.addEdge('Seoul', 'Hawaii')
-
+// g.addVertex('Tokyo')
+// g.addVertex('Seoul')
+// g.addVertex('America')
+// g.addVertex('Hawaii')
+// g.addEdge('Tokyo', 'Hawaii')
+// g.addEdge('Tokyo', 'Seoul')
+// g.addEdge('Hawaii', 'America')
+// g.addEdge('Seoul', 'Hawaii')
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")                           
+g.addVertex("D")                       
+g.addVertex("E")                       
+g.addVertex("F")                       
+                    //      A
+                    //    /   \
+                    //   B     C
+                    //  /       \
+                    // D---------E
+                    // \         /
+                    //  \       /
+                    //      F                       
+g.addEdge("A","B")                    
+g.addEdge("A","C")                     
+g.addEdge("B","D")                         
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
