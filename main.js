09b17afdfc712c25cf895718206b8640d7d3fbb9
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-04-02 13:32:49 -0700

    added in BFS in graph

--- main.js
+++ main.js
@@ -1026,6 +1026,27 @@ class Graph{
     }
     return result;
   }
+
+  breathFirstSearch(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while(queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if(!visited[neighbor]){
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      })
+    }
+    return result;
+  }
 }
 
 
